d846c92b1b440b01796eeb6a9c8e7701fed51857
Pedro H. <pedro.26.3.h@gmail.com>
2024-04-13 19:47:28 -0300

    Exercício 3

---
+++ 3.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef __3_h__
+#define __3_h__
+
+/* Inclusões */
+#include <stdio.h>
+
+/* Funções */
+void ex03()
+{
+    int a = 9, b = 128, c = 49, d = 100, e = 13, f = 200; // (D)uzentos :)
+
+    printf("a) %d\n\t"
+            "b) %d\n\t"
+            "c) %d\n\t"
+            "d) %d\n\t"
+            "e) %d\n\t"
+            "f) %d\n", a, b, c, d, e, f);
+}
+
+#endif
