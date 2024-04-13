47833ff6ddb7400ee27fa2f6146f14e6bf8d837f
Pedro H. <pedro.26.3.h@gmail.com>
2024-04-13 19:47:20 -0300

    Exercício 1

---
+++ 1.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef __1_h__
+#define __1_h__
+
+/* Inclusões */
+#include <stdio.h>
+
+/* Funções */
+void ex01()
+{
+    int INDICE = 13, SOMA = 0, K = 0; 
+
+    while (K < INDICE)
+    {
+        K = K + 1;
+        SOMA = SOMA + K;   
+    }
+    printf("%d", SOMA);
+}
+
+#endif
